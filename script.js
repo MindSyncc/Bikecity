2c2a94223d5f7e27cb374ed9a095977344834f75
Juan <juanfuentesrufino6@gmail.com>
2024-04-29 15:38:05 -0300

    Adicionando as imagens ao projeto, criando as funcoes para automatizar o slideshow e a mudança do background. Commit feito por Juan Fuentes Rufino, link do github https://github.com/JuanRufino63

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 alert("Seja bem-vindo!");
+let imagens = ['img/Bicicleta1.jpg', 'img/Bicicleta2.jpg', 'img/Bicicleta3.jpg', 'img/Bicicleta4.jpg'];
+let index = 0;
+let time = 3000;
+
+function slideShow() {
+    document.getElementById('imgSlide').src=imagens[index];
+    index++;
+
+    if(index == imagens.length) {
+        index = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
+function mudarCor(){
+    function mudar(numero){
+        return(Math.random()* numero);
+    }
+    const cores = `rgb(${mudar(255)},${mudar(255)},${mudar(255)})`;
+    document.body.style.backgroundColor=cores;
+}
+window.addEventListener('load', function(){
+    this.setInterval(function(){
+        mudarCor();
+    },2000)
+})
